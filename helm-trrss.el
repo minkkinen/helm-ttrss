d10c55eaf8ef5e28a5c035e00a14ba7d7b8a4672
Matti Minkkinen <matti.minkkinen@iki.fi>
2016-02-20 15:04:23 +0200

    Add function to open article.

--- helm-trrss.el
+++ helm-trrss.el
@@ -4,7 +4,8 @@
   '((name . "TTRSS starred articles")
     (init . helm-ttrss-init)
     (candidates . helm-ttrss-candidates)
-    (action . (("Unstar" . helm-ttrss-unstar))))
+    (action . (("Unstar" . helm-ttrss-unstar)
+	       ("Open" . helm-ttrss-open))))
   "Source for starred articles in TTRSS.")
 
 (defun helm-ttrss-init ()
@@ -24,6 +25,19 @@
 	       (first (ttrss-get-headlines ttrss-address ttrss-sid :feed_id -1 :search search-string))
 	       :id)))
 	(ttrss-update-article ttrss-address ttrss-sid article-id :mode 0 :field 0)))))
+
+(defun helm-ttrss-open (_)
+  (let* ((ttrss-sid (ttrss-login ttrss-address ttrss-user ttrss-password))
+	 (search-string (first (helm-marked-candidates)))
+	 (article-id
+	  (plist-get
+	   (first (ttrss-get-headlines ttrss-address ttrss-sid :feed_id -1 :search search-string)) :id)))
+    (with-temp-buffer
+      (let ((article (first (ttrss-get-article ttrss-address ttrss-sid article-id))))
+	(insert (concat "<h1>" (plist-get article :title) "</h1>"))
+	(insert "\n")
+	(insert (plist-get article :content))
+	(shr-render-buffer (current-buffer))))))
 
 (defun helm-ttrss-starred ()
   (interactive)
