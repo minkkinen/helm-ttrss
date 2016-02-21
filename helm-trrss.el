673d8f40a89f0db98ad01c9787a74740b08703ba
Matti Minkkinen <matti.minkkinen@iki.fi>
2016-02-21 14:40:37 +0200

    Match articles by id rather than imprecise search.

--- helm-trrss.el
+++ helm-trrss.el
@@ -14,25 +14,24 @@
 (defun helm-ttrss-candidates ()
   (let* ((ttrss-sid (ttrss-login ttrss-address ttrss-user ttrss-password))
 	 (headlines (ttrss-get-headlines ttrss-address ttrss-sid :feed_id -1)))
-    (mapcar (lambda (x) (plist-get x :title)) headlines)))
+    (mapcar (lambda (x)
+	      (concat "|" (number-to-string (plist-get x :id)) "| "
+		      (plist-get x :title)))
+	    headlines)))
 
 (defun helm-ttrss-unstar (_)
   (let* ((ttrss-sid (ttrss-login ttrss-address ttrss-user ttrss-password)))
     (dolist (i (helm-marked-candidates))
-      (let* ((search-string i)
-	     (article-id
-	      (plist-get 
-	       (first (ttrss-get-headlines ttrss-address ttrss-sid :feed_id -1 :search search-string))
-	       :id)))
+      (let* ((id-match (string-match "^|\\([0-9]+\\)|" i))
+	     (article-id (match-string-no-properties 1 i)))
 	(ttrss-update-article ttrss-address ttrss-sid article-id :mode 0 :field 0))))
   (helm-refresh))
 
 (defun helm-ttrss-open (_)
   (let* ((ttrss-sid (ttrss-login ttrss-address ttrss-user ttrss-password))
-	 (search-string (first (helm-marked-candidates)))
-	 (article-id
-	  (plist-get
-	   (first (ttrss-get-headlines ttrss-address ttrss-sid :feed_id -1 :search search-string)) :id)))
+	 (first-candidate (first (helm-marked-candidates)))
+	 (id-match (string-match "^|\\([0-9]+\\)|" first-candidate))
+	 (article-id (string-to-number (match-string-no-properties 1 first-candidate))))
     (with-temp-buffer
       (let ((article (first (ttrss-get-article ttrss-address ttrss-sid article-id))))
 	(insert (concat
